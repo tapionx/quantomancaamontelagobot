5a1ac6fb17568659f9099e6f1a94007bc2ab47c6
Riccardo Serafini <riccardo.serafini@plentific.com>
2023-08-09 00:42:28 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.11-slim-buster
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV TZ=Europe/Rome
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+COPY . .
+
+CMD ["python3", "bot.py"]
---
+++ bot.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+TOKEN = os.getenv("TOKEN")
+
+MONTELAGO = datetime.datetime(2024, 7, 31, 16, 0, 0)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Ciao! Usa il comando /quanto_manca per sapere quanto manca al Montelago Celtic Festival")
+
+async def quanto_manca(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(update)
+    now = datetime.datetime.now()
+    if MONTELAGO < now:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="CI SIAMO!")
+        return
+    delta = MONTELAGO - now
+    delta_text = 'Mancano {} giorni, {} ore, {} minuti e {} secondi al <a href="https://www.montelagocelticfestival.it/">Montelago Celtic Festival</a>'.format(delta.days, delta.seconds // 3600, (delta.seconds // 60) % 60, delta.seconds % 60)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text=delta_text,
+        parse_mode='HTML',
+        disable_web_page_preview=True,
+    )  
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    application.add_handler(CommandHandler("quanto_manca", quanto_manca))
+    
+    application.run_polling()
